7f1b107ede355598059d96d91a9e7b02f6fba7bd
Philipp Daniels <philipp.daniels@gmail.com>
2016-10-19 20:40:09 +0200

    Add alpine image for 2.1

---
+++ 2.1/alpine/install.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+set -o errexit
+set -o pipefail
+set -o nounset
+
+main() {
+  install
+  cleanup
+  addDefaultCerts "vagrant/conf/ca_bundle"
+}
+
+install() {
+  echo "=> Download portus and install gems"
+  wget -qO- "https://github.com/SUSE/Portus/archive/${PORTUS_VERSION}.tar.gz" | tar -xvz --strip-components=1
+
+  # Fix missing gems without dev
+  sed -i '/gem "md2man"/d' ${BUNDLE_GEMFILE}
+  sed -i '/gem "puma"/d' ${BUNDLE_GEMFILE}
+  echo -e "gem \"md2man\", \"~>5.1.1\", require: false\ngem \"puma\"" >> ${BUNDLE_GEMFILE}
+
+  bundle config --local build.nokogiri --use-system-libraries # Fix broken nokogiri build
+  bundle install --jobs=4 --retry=3 --no-cache --clean --deployment --without test development
+}
+
+cleanup() {
+  echo "=> Remove build dependencies and cleanup tmp/cache"
+  gem cleanup
+  rm -rf /tmp/* /tmp/.??* /root/* /root/.??* /usr/lib/ruby/gems/*/cache/*
+}
+
+addDefaultCerts() {
+  echo "=> Provide default SSL certs"
+
+  src_folder="${1}"
+  mkdir ${PORTUS_CONFIG_PATH}/certs
+  cp ${src_folder}/server.key ${src_folder}/server.crt ${PORTUS_CONFIG_PATH}/certs
+}
+
+main
---
+++ 2.1/alpine/run.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env sh
+set -o errexit
+set -o pipefail
+set -o nounset
+
+main() {
+  debug "Create database"
+  rake db:create
+
+  debug "Migrate database schema"
+  rake db:migrate
+
+  bundle exec puma &
+  puma_pid="$!"
+  debug "Starting webserver - ${puma_pid}"
+
+  if [ -n "${PORTUS_CATALOG_CRON:-}" ]; then
+    export "CATALOG_CRON=${PORTUS_CATALOG_CRON:-}"
+  fi
+  bundle exec crono &
+  crono_pid="$!"
+  debug "Starting synchronisation - ${crono_pid}"
+
+  wait ${puma_pid}
+  kill -s SIGKILL ${crono_pid}
+}
+
+debug() {
+  message="${1:-}"
+  if [ "${DEBUG:-0}" -eq "1" ]; then
+    echo "[$(date +'%F %X %z')]: $message"
+  fi
+}
+
+shutdown() {
+  debug "Shutdown portus - ${puma_pid} ${crono_pid}"
+  kill -s SIGTERM ${puma_pid}
+  kill -s SIGKILL ${crono_pid}
+  sleep 1
+  exit 0
+}
+
+
+puma_pid=""
+crono_pid=""
+trap shutdown SIGINT SIGTERM
+main
